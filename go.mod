172946059ee2ff3cd93a90b40632d7e67848dea1
Loki Verloren <stalker.loki@protonmail.ch>
2021-04-30 18:41:11 +0200

    updated version

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/p9c/opts
 go 1.16
 
 require (
-	github.com/p9c/log v0.0.6
+	github.com/p9c/log v0.0.12
 	go.uber.org/atomic v1.7.0
 	gopkg.in/src-d/go-git.v4 v4.13.1
 )
+
+replace	github.com/p9c/log => ../log
--- version/version.go
+++ version/version.go
@@ -13,12 +13,12 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname
 	GitRef = "refs/heads/main"
 	// GitCommit is the commit hash of the current HEAD
-	GitCommit = "69697a82d22baa9b5a8920c5602c7ec670c7db7c"
+	GitCommit = "4bc9c6f383ddf9108bf99b0c5eee0474fd0d162d"
 	// BuildTime stores the time when the current binary was built
-	BuildTime = "2021-04-30T18:21:50+02:00"
+	BuildTime = "2021-04-30T19:56:39+02:00"
 	// Tag lists the Tag on the build, adding a + to the newest Tag if the commit is
 	// not that commit
-	Tag = "v0.0.10+"
+	Tag = "v0.0.12"
 	// PathBase is the path base returned from runtime caller
 	PathBase = "/home/loki/src/github.com/p9c/pod/pkg/opts/"
 	// Major is the major number from the tag
@@ -26,7 +26,7 @@ var (
 	// Minor is the minor number from the tag
 	Minor = 0
 	// Patch is the patch version number from the tag
-	Patch = 10
+	Patch = 12
 	// Meta is the extra arbitrary string field from Semver spec
 	Meta = ""
 )
